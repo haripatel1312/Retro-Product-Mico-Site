6df31e63fc6b91700d9dc99ac3fd40f4de728912
Hari Patel <hp94944@gmail.com>
2023-08-05 19:06:09 -0400

    First Commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // Load header
+    $('#global-header').load('header.html', function() {
+        // Header loaded callback, if needed
+    });
+
+    // Load footer
+    $('#global-footer').load('footer.html', function() {
+        // Footer loaded callback, if needed
+    });
+});
